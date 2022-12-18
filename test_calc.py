b92551e44cbd49349cb3ee27a64347ae1ed58fda
Тимофей <play-t@bk.ru>
2022-12-18 14:36:54 +0400

    Add files via upload

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+import pytest
+from Calculator import Calculator
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator()
+    def test_success(self):
+        assert self.calc.adding(1, 1) == 2
+    def test_unsuccess(self):
+        assert self.calc.adding(1, 1) == 3
+    def test_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(1, 0)
+    def teardown(self):
+        print('Выполнение методае Teardown')
